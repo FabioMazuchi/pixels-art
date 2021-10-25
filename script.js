89089827f5a8cf0b68ae60b3493266ed6c21b441
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-10-25 16:04:01 -0300

    adiciona requisito 6

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let quadroPixel = document.querySelector('#pixel-board');
-console.log(quadroPixel);
-for (let i = 0; i < 25; i++) {
-	let divPixel = document.createElement('div');
-	divPixel.className = 'pixel';
-	quadroPixel.appendChild(divPixel);
-	
 
-	
+function criaQuadroPixel() {
+	for (let i = 0; i < 25; i++) {
+		let divPixel = document.createElement('div');
+		divPixel.className = 'pixel';
+		quadroPixel.appendChild(divPixel);
+	}
+}
+
+criaQuadroPixel();
+
+window.onload = function(){
+		let cores = document.querySelectorAll('.color');
+		let selecionada = 'selected';
+		cores[0].classList.add('selected');
+		console.log(cores);
 }
