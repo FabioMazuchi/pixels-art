e793917f7f8d622ad7bdb4dc804e9a740ecb4add
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-10-25 18:55:27 -0300

    corrige requisito 7

--- script.js
+++ script.js
@@ -10,21 +10,22 @@ function criaQuadroPixel() {
 
 criaQuadroPixel();
 
-window.onload = function(){
-		let cores = document.querySelectorAll('.color');
-		cores[0].classList.add('selected');
-	
-		for(let c of cores){
-			c.addEventListener('click', function () {
-				for (let index = 0; index < cores.length; index++) {
-					if(cores[index].classList.length > 1){
-						cores[index].classList.remove('selected');
-						console.log(cores[index]);
-					}else{
-						this.classList.add('selected');
-					}
+window.onload = function () {
+	let cores = document.querySelectorAll('.color');
+	cores[0].classList.add('selected');
+
+	for (let c of cores) {
+		c.addEventListener('click', function () {
+			let clicou = this.innerText;
+			console.log(clicou);
+			this.classList.add('selected');
+			for (let index = 0; index < cores.length; index += 1) {
+				console.log(cores[index].innerText);
+				if(cores[index].innerText !== clicou){
+					cores[index].classList.remove('selected');
 				}
-			});
-		}
+			}
+		});
+	}
 }
 
