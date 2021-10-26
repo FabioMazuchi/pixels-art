58b99c5b1c4e39a250b21a6ff176875dab5b98fc
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-10-26 04:39:25 -0300

    adiciona requisito 8

--- script.js
+++ script.js
@@ -4,28 +4,60 @@ function criaQuadroPixel() {
 	for (let i = 0; i < 25; i++) {
 		let divPixel = document.createElement('div');
 		divPixel.className = 'pixel';
+		divPixel.innerText = i;
 		quadroPixel.appendChild(divPixel);
 	}
 }
 
 criaQuadroPixel();
 
-window.onload = function () {
+function adicionaClsseSelected(){
 	let cores = document.querySelectorAll('.color');
 	cores[0].classList.add('selected');
 
 	for (let c of cores) {
 		c.addEventListener('click', function () {
 			let clicou = this.innerText;
-			console.log(clicou);
 			this.classList.add('selected');
+
 			for (let index = 0; index < cores.length; index += 1) {
-				console.log(cores[index].innerText);
-				if(cores[index].innerText !== clicou){
+				if (cores[index].innerText !== clicou) {
 					cores[index].classList.remove('selected');
+					selecionaCor();
+					pintarPixel();
 				}
+			}
+
+			return clicou;
+		});
+	}
+}
+
+adicionaClsseSelected();
+
+function selecionaCor(){
+	let selecionada = document.querySelector('.selected');
+	return selecionada.innerHTML;
+}
+
+
+function pintarPixel(){
+	let pixels = document.querySelectorAll('.pixel');
+	let corSelecionada = selecionaCor();
+	
+	for(let i = 0; i < pixels.length; i+=1){
+		pixels[i].addEventListener('click', function(){
+			let selecionado = this.innerText;
+			let estilo = window.getComputedStyle(pixels[i]);
+			corAtual = estilo.backgroundColor;
+
+			if(i !== selecionado){
+				pixels[i].style.backgroundColor = corSelecionada;
+			}else{
+				pixels[i].style.backgroundColor = corAtual;
 			}
 		});
 	}
 }
 
+pintarPixel();
